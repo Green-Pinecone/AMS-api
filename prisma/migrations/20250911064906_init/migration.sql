3c066c14a97e7835c0ac5f9284e7d0fa74ae3011
Green-Pinecone <sangbadmazumder78@gmail.com>
2025-09-11 13:35:09 +0530

    Employee

--- prisma/migrations/20250911064906_init/migration.sql
+++ prisma/migrations/20250911064906_init/migration.sql
@@ -4,7 +4,18 @@ BEGIN TRAN;
 
 -- CreateTable
 CREATE TABLE [dbo].[Employee] (
-    [first_name] char
+    [first_name] varchar(255) NOT NULL,
+    [last_name] varchar(255) NOT NULL,
+    [emp_id] int NOT NULL IDENTITY,
+    [dob] datetime2(7) NOT NULL,
+    [gender] varchar(255) NOT NULL,
+    [personal_email] varchar(255) NOT NULL,
+    [work_email] varchar(255) NOT NULL,
+    [contact_no] varchar(255) NOT NULL,
+    [address] varchar(255) NOT NULL,
+    [hire_date] datetime2(7) NOT NULL,
+    [employment_status] varchar(255) NOT NULL,
+    [employment_type] varchar(255) NOT NULL
 );
 
 COMMIT TRAN;
